d9bdd4e780ef92a63072f823b32fce8b375dd39e
NBelleBee <bokolobeneditte@gmail.com>
2026-08-12 14:15:27 +0100

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,11 +1,16 @@
-```tsx
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { createClient } from '@supabase/supabase-js'
 import type { Database } from './types'
 
 export const createServerSupabase = () => {
-  return createServerComponentSupabaseClient<Database>({
-    cookies,
-  })
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY
+
+  if (!supabaseUrl || !supabaseSecretKey) {
+    throw new Error('Supabase server environment variables are missing.')
+  }
+
+  return createClient<Database>(
+    supabaseUrl,
+    supabaseSecretKey
+  )
 }
-```
